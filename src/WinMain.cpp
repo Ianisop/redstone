a21140a4dd1ef1ac1efb71fd290d8012a2fa2920
Ianisop <69714456+Ianisop@users.noreply.github.com>
2024-01-08 16:27:04 +0100

    init

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <wingdi.h>
+#include <algorithm>
 
 #pragma comment(lib, "dwmapi.lib")
 #include <dwmapi.h>
@@ -26,12 +27,13 @@
 #pragma comment (lib, "d3dcompiler.lib")
 
 // include Lapis headers
-#include "Lapis/Engine.h"
-#include "Lapis/Helpers.h"
+#include "engine/LapisEngine.h"
+#include "engine/GlobalDefines.h"
+#include "engine/Helpers.h"
 
 // include Utility headers
 #include "utils/hsl-to-rgb.hpp"
-
+#include "Game.h"
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
@@ -93,13 +95,12 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 #endif
 
     printf("initting lapis\n");
-    Lapis::InitLapis(nullptr);
-
+    Lapis::InitLapis(hwnd);
 
     float FPS_CAP = 60;
     bool LIMIT_FPS = false;
     MSG msg{};
-    while (true)// && !GetAsyncKeyState(VK_DELETE))
+    while (true && !GetAsyncKeyState(VK_DELETE))
     {
         using namespace Lapis;
         using namespace Lapis::Draw;
@@ -114,7 +115,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         if (fpsSamples > 0) {
             fps = 1000/(1000 * fpsTimeTotal / fpsSamples);
         }
-        std::cout << std::format("delta:   {:.8f}s\nelapsed: {:.4f}s\navg fps: {}\n", Lapis::deltaTime, Lapis::elapsedTime, fps);
+        //std::cout << std::format("delta:   {:.8f}s\nelapsed: {:.4f}s\navg fps: {}\n", Lapis::deltaTime, Lapis::elapsedTime, fps);
 
         if (GetAsyncKeyState('A')) mainCamera.pos += Vec3::right * deltaTime;
         if (GetAsyncKeyState('D')) mainCamera.pos -= Vec3::right * deltaTime;
@@ -123,10 +124,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         if (GetAsyncKeyState('W')) mainCamera.pos -= Vec3::forward * deltaTime;
         if (GetAsyncKeyState('S')) mainCamera.pos += Vec3::forward * deltaTime;
 
-        if (GetAsyncKeyState(VK_RIGHT)) mainCamera.rot += Vec3(0, 45, 0) * deltaTime;
-        if (GetAsyncKeyState(VK_LEFT))  mainCamera.rot -= Vec3(0, 45, 0) * deltaTime;
-        if (GetAsyncKeyState(VK_UP))    mainCamera.rot -= Vec3(45, 0, 0) * deltaTime;
-        if (GetAsyncKeyState(VK_DOWN))  mainCamera.rot += Vec3(45, 0, 0) * deltaTime;
 
         Lapis::NewFrame();
 
@@ -143,11 +140,11 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         //D2::Rect(Vec2(10,100), Vec2(50,10), {0,0,1,1});
         //D2::Circle(Vec2(120), 20, { 0,1,0,1 });
         //D2::Circle(Vec2(150), 20, { 0,0,0,1 }, 12);
-
-        //D3::Plane(Transform(Vec3::forward + -Vec3::up * 0.3f, 0, 1), {1,1,1,1});
-        //D3::Cube(Transform(Vec3::forward * 3, 0, 1 ), { 0.92, 0.26, .27, 1});
-        //D3::Line(Vec3::forward * 3 + -Vec3::right, Vec3::forward * 2 + -Vec3::right * 1.5, { 0.345, 0.396, 0.949, 1});
-        //D3::Arrow(Vec3::forward * 1.5, Vec3(.1), { 0.996, 0.906, 0.361, 1 });
+       
+        D3::Plane(Transform(Vec3::forward + -Vec3::up * 0.3f, 0, 1), {1,1,1,1});
+        D3::Cube(Transform(Vec3::forward * 3, 0, 1 ), { 0.92, 0.26, .27, 1});
+        D3::Line(Vec3::forward * 3 + -Vec3::right, Vec3::forward * 2 + -Vec3::right * 1.5, { 0.345, 0.396, 0.949, 1});
+        D3::Arrow(Vec3::forward * 1.5, Vec3(.1), { 0.996, 0.906, 0.361, 1 });
 
         Lapis::RenderFrame();
         Lapis::FlushFrame();
@@ -170,6 +167,34 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
         PostQuitMessage(0);
         return 0;
         break;
+    
+    case WM_MOUSEMOVE:
+        {
+            using namespace Lapis;
+
+            static int xRot{}, yRot{};
+            static int xPosOld{ GET_X_LPARAM(lParam) }, yPosOld{ GET_Y_LPARAM(lParam) };
+
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+
+            xRot += xPos - xPosOld;
+            yRot += yPos - yPosOld;
+            //std::cout << xPos - xPosOld << "\n";
+            yRot = std::clamp(yRot, -90, 90);
+        
+            std::cout << std::format("{} x, {} y \n", xRot, yRot);
+            mainCamera.rot = Vec3(yRot, xRot, 0);
+
+            xPosOld = xPos;
+            yPosOld = yPos;
+            ShowCursor(false);
+
+            break;
+        }
+
+
+
     }
     // Handle any messages the switch statement didn't
     return DefWindowProc(hWnd, message, wParam, lParam);
---
+++ src/engine/GlobalDefines.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define SCREEN_WIDTH (1600)
+#define SCREEN_HEIGHT (900)
+#define FOV RAD2DEG * 45
+#define CLEAR_RENDER_TARGET_VIEW 1
+#define CLEAR_DEPTH_STENCIL_VIEW 1
+
+#define USE_Z_UP 0
+
