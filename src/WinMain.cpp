59bcbf6e732788f2230eef5fe07591e72ed44c7d
Ianisop <69714456+Ianisop@users.noreply.github.com>
2024-01-12 13:19:12 +0100

    wow

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -147,8 +147,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         {
             using namespace Lapis;
             NewFrame();
-
-
             RenderFrame();
             FlushFrame();
         }
